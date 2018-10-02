d62a772ef3ad9225a626e9dc977880e60527d672
ryanmcc13 <ryanmccullough21@gmail.com>
2018-10-01 21:08:03 -0500

    fixes to constructor and bloomfilter creator

--- src/main/coms435/pa1/differential/BloomDifferential.java
+++ src/main/coms435/pa1/differential/BloomDifferential.java
@@ -2,29 +2,23 @@ package main.coms435.pa1.differential;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-import main.coms435.pa1.filter.BloomFilterRan;
 import main.coms435.pa1.filter.BloomFilter;
-import main.coms435.pa1.filter.BloomFilterFNV;
-import main.coms435.pa1.filter.BloomFilterMurmur;
+
 
 public class BloomDifferential
 {
     private BloomFilter filter;
-    private int filterType;
     private String diffPath;
   //bits per element at 4, 8, or 10
-    private int bitsPerElement;
     private String databasePath;
     
     
-    public BloomDifferential(int filterType, String diffPath, int bitsPerElement, String databasePath)
+    public BloomDifferential(BloomFilter filter, String diffPath, String databasePath)
     {
-    	this.filterType = filterType;
+    	this.filter = filter;
     	this.diffPath = diffPath;
-    	this.bitsPerElement = bitsPerElement;
     	this.databasePath = databasePath;
     }
 
@@ -32,7 +26,6 @@ public class BloomDifferential
     {
     	//or path to the file
     	File file = new File(diffPath);
-    	ArrayList<String> keys = new ArrayList<String>();
     	try {
 			Scanner diffScanner = new Scanner(file);
 			while(diffScanner.hasNextLine()) {
@@ -51,27 +44,13 @@ public class BloomDifferential
 					charCount++;
 				}
 				String key = s.substring(0, charCount-4);
-				keys.add(key);
+				filter.add(key);
 			}
 			diffScanner.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Could not find DiffFile.txt");
 			e.printStackTrace();
 		}
-    	
-    	
-    	if(filterType == 1) {
-    		filter = new BloomFilterFNV(keys.size(),bitsPerElement);
-    	}else if(filterType == 2) {
-    		filter = new BloomFilterMurmur(keys.size(),bitsPerElement);
-    	}else {
-    		filter = new BloomFilterRan(keys.size(),bitsPerElement);
-    	}
-    	
-    	//add keys to filter
-    	for(int i = 0; i<keys.size();i++) {
-    		filter.add(keys.get(i));
-    	}
     	
     	return filter;
     }
