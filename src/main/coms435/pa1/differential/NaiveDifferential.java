e8fd0b8cf460dbd0e68938602e9de168ca89f33e
ryanmcc13 <ryanmccullough21@gmail.com>
2018-10-02 21:42:54 -0500

    stores diff file in main memory

--- src/main/coms435/pa1/differential/NaiveDifferential.java
+++ src/main/coms435/pa1/differential/NaiveDifferential.java
@@ -3,36 +3,50 @@ package coms435.pa1.differential;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 public class NaiveDifferential
 {
 	private String databasePath;
-	private String diffPath;
+	private HashMap<String, String> map = new HashMap<String, String>();
 
 	public NaiveDifferential(String diffPath, String databasePath)
     {
-
-    	this.diffPath = diffPath;
     	this.databasePath = databasePath;
-    }
-
-	public Object retrieveRecord(String key) {
-		try {
+    	
+    	try {
 			BufferedReader br = new BufferedReader(new FileReader(diffPath));
     		String line = "";
 			while((line=br.readLine())!=null) {
-				if(line.contains(key)) {
-					br.close();
-					return line;
+				int numCount = 0;
+				int charCount = 0;
+				for(int i = 0; i<line.length();i++) {
+					if(Character.isDigit(line.charAt(i))){
+						numCount++;
+					}else {
+						numCount = 0;
+					}
+					if(numCount == 4) {
+						break;
+					}
+					charCount++;
 				}
+				String key = line.substring(0, charCount-4);
+				map.put(key, line);
 			}
 			br.close();
 		} catch (IOException e) {
 			System.out.println("Couldn't find file");
 			e.printStackTrace();
 		}
-			//or path to database.txt
+    }
+
+	public Object retrieveRecord(String key) {
+		String record = map.get(key);
+		if (record != null) {
+			return record;
+		}
 		try {	
 			BufferedReader brDB = new BufferedReader(new FileReader(databasePath));
     		String line = "";
